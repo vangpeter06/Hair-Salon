7f3696b2e92f4ac837b3474613082347ef251e38
Peter Vang <vang_peter06@yahoo.com>
2022-07-29 10:18:40 -0700

    add client model

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,17 @@
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string StyleHair { get; set; }
+
+        public int StylistId { get; set; }
+
+        public virtual Stylist Stylist { get; set; }
+    }
+}
