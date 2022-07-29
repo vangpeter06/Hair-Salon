f6551d21c11803cb1423dc307d9e0fe831e04f5e
Peter Vang <vang_peter06@yahoo.com>
2022-07-29 10:13:51 -0700

    add hair salon context

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+    public class HairSalonContext : DbContext
+    {
+        public virtual DbSet<Stylist> Stylists { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public HairSalonContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
